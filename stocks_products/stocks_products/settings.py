d6b48a272d82d0c7dbf539bee9db3fc72b3a5e65
97wave <e97.wav@gmail.com>
2021-09-22 16:58:53 +0300

    2

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-
+    'django_filters',
     'logistic',
 ]
 
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6    
+    'PAGE_SIZE': 2    
     }
